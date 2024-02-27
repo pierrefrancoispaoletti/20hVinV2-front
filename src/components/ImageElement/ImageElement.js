1c06f25231ef9901a5dd8aabd069cfe0e6a726bb
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2024-02-27 19:43:05 +0100

    Updates

--- src/components/ImageElement/ImageElement.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { placeLocation } from "../../_const";
-
-const ImageElement = ({ width, image, alt }) => {
-  return <img width={width} src={image} alt={alt} />;
-};
-
-export default ImageElement;
